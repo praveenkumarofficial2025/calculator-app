f026099614d04939601f06115ff04d04ce8fd40f
Praveen Kumar New <praveenkumarofficial.2025@gmail.com>
2025-08-22 12:14:08 +0530

    added Division class

--- src/main/java/CalculatorApp.java
+++ src/main/java/CalculatorApp.java
@@ -1,6 +1,26 @@
 public class CalculatorApp {
 
     public static void main(String[] args) {
-        System.out.println("welcome to java");
+
+        if(args!=null && args.length==3){
+
+            String calType = args[0];
+            int x = Integer.parseInt(args[1]);
+            int y = Integer.parseInt(args[2]);
+            int reult = 0;
+
+            if(calType.equals("+"))
+                reult= Addition.cal(x,y);
+
+            else if (calType.equals("-")) {
+                reult= Substraction.cal(x,y);
+            }
+            else if (calType.equals("*")) {
+                reult= Multiplication.cal(x,y);
+            }
+
+            System.out.println("result="+reult);
+
+        }
     }
 }
